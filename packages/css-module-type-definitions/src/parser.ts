d616264769d667d369a6cc0220749e8b9bf3241a
Phil Hill <phil@technobuddha.com>
2020-11-12 15:54:13 -0500

    Upgrade packages

--- packages/css-module-type-definitions/src/parser.ts
+++ packages/css-module-type-definitions/src/parser.ts
@@ -1,16 +1,16 @@
 import fs                   from 'fs-extra';
-import postcss              from 'postcss';
 import { cosmiconfigSync }  from 'cosmiconfig';
+import postcss, { AcceptedPlugin, Parser, Root } from 'postcss';
 
 // TODO this could proably use some better type definitions, but I can't find them :(
 const explorer  = cosmiconfigSync('postcss');
 const found     = explorer.search();
 const config    = found!.config;
 
-export const importer       = require('postcss-import')(config.plugins['postcss-import'])   as postcss.AcceptedPlugin;
-export const localByDefault = require('postcss-modules-local-by-default')                   as postcss.AcceptedPlugin;
-export const scope          = require('postcss-modules-scope')                              as postcss.AcceptedPlugin;
-export const comment        = require('postcss-comment')                                    as postcss.Parser;                                    
+export const importer       = require('postcss-import')(config.plugins['postcss-import'])   as AcceptedPlugin;
+export const localByDefault = require('postcss-modules-local-by-default')                   as AcceptedPlugin;
+export const scope          = require('postcss-modules-scope')                              as AcceptedPlugin;
+export const comment        = require('postcss-comment')                                    as Parser;                                    
 
 export const defaultPlugins = [ importer, localByDefault, scope ];
 
@@ -18,11 +18,11 @@ export type ParserReturn    = Set<string>;
 
 export async function parser(
     filePath:   string,
-    plugins:    postcss.AcceptedPlugin[] | null = null,
+    plugins:    AcceptedPlugin[] | null = null,
 ):  Promise<ParserReturn> {
     const exportTokens  = new Set<string>();
 
-    const gatherPlugIn =  (root: postcss.Root) => {
+    const gatherPlugIn =  (root: Root) => {
         root.each(
             node => {
                 if (node.type === 'rule' && node.selector === ':export') {
