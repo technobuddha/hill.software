01544ecf1416bcd7dec115c6fbed5b7afa080193
Phil Hill <phil@technobuddha.com>
2021-03-11 10:11:41 -0500

    Publish

--- packages/react-hooks/src/useWindowSize.ts
+++ packages/react-hooks/src/useWindowSize.ts
@@ -3,16 +3,17 @@ import throttle          from 'lodash/throttle';
 import { measureWindow } from '@technobuddha/library/measure';
 
 export function useWindowSize() {
-    const [ size, setSize ] = React.useState(measureWindow());
+    const [ count, setCount ]   = React.useState(0);
+    const [ size, setSize ]     = React.useState(measureWindow());
 
     React.useLayoutEffect(() => {
-        const updateSize = throttle(() => { setSize(measureWindow()); });
+        const updateSize = throttle(() => { setSize(measureWindow()); setCount(c => c + 1); });
         window.addEventListener('resize', updateSize);
 
         return () => { window.removeEventListener('resize', updateSize); };
     });
 
-    return size;
+    return { ...size, count };
 }
 
 export default useWindowSize;
